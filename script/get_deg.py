7b54f969be02d6b48c70d00f785e02ebe848a46e
estading <estading@nebrwesleyan.edu>
2022-05-24 15:00:22 -0400

    get_deg.py includes try and except

--- script/get_deg.py
+++ script/get_deg.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32
-
-if __name__ == '__main__':
-	rospy.init_node('get_deg')
+def pull_degree():
+	rospy.init_node('get_deg', anonymous=True)
 	pub = rospy.Publisher('/deg_topic', Float32, queue_size = 10)
-	
 	rate = rospy.Rate(0.2)
 	while not rospy.is_shutdown():
 		msg = Float32()
 		msg.data = rospy.get_param("/degrees")
 		pub.publish(msg)
-		rate.sleep()
-	rospy.loginfo('Node stopped')
+		rate.sleep()	
+
+if __name__ == '__main__':
+	try:
+		pull_degree()
+	except rospy.ROSInterruptException:
+		pass
